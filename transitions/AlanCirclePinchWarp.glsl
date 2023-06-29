e8e288cad6c74e1e4477e7b69d562b3fbe12ea98
Alan Cushway <acushway@hotmail.com>
2023-06-29 17:10:04 +1000

    Update AlanCirclePinchWarp.glsl

--- transitions/AlanCirclePinchWarp.glsl
+++ transitions/AlanCirclePinchWarp.glsl
@@ -13,12 +13,10 @@ vec4 transition(float ratio, in vec2 st) {
     
     float w = 0.3;
     vec2 p1 = p*(1.+smoothstep(0., l+w, ratio));
-    vec2 p2 = p*smoothstep(l-w, 1./*smoothstep(l+w, 1., ratio)*/, ratio);
+    vec2 p2 = p*smoothstep(l-w, 1., ratio);
     
     vec2 uv1 = p1*0.5+0.5;
     vec2 uv2 = p2*0.5+0.5;
-    // return vec4(l, 0., 0., 1.);
-    // return vec4(step(0.99, l), 0., 0., 1.);
     return mix(getFromColor(uv1), getToColor(uv2), ll);
 }
 
